473572bd3bb8bd47e55ef015cd7713eaa68fabd5
jason-co <jason.christopher.ware@gmail.com>
2015-03-17 17:43:21 -0700

    Initial check in of the project

---
+++ MVC.Series.ViewModel/BookViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVC.Series.ViewModel
+{
+	public class BookViewModel
+	{
+		public BookViewModel()
+		{
+			Publisher = new PublisherViewModel();
+
+			Characters = new List<CharacterViewModel>();
+			for ( int i = 0; i < 3; i++ )
+			{
+				Characters.Add( new CharacterViewModel() );
+			}
+		}
+
+		public BookViewModel( string title, string author, string bookCoverUrl )
+		{
+			Title = title;
+			Author = author;
+			BookCoverUrl = bookCoverUrl;
+		}
+
+		public string Title { get; set; }
+		public string Author { get; set; }
+		[Display( Name = "Book Cover" )]
+		public string BookCoverUrl { get; set; }
+
+		public PublisherViewModel Publisher { get; set; }
+		public IList<CharacterViewModel> Characters { get; set; }
+	}
+}
